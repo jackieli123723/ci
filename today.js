5193ed4a6439c05a4570f32a57fdfde5bcb4b09b
jackieli123723 <380863274@qq.com>
2019-11-08 08:22:50 +0800

    更新今天数据接口

--- today.js
+++ today.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 4003
 const axios = require('axios')
 const cheerio = require('cheerio')
 const city = require('./city/index.js')
-axios.defaults.headers.post['Content-Type'] = 'text/html';
 
 const utils = require('./config/utils');
 const trim = utils.trim;
@@ -38,36 +37,45 @@ app.use(async (ctx, next) => {
   }
 })
 
+// http://www.weather.com.cn/weather1dn/101010100.shtml
+//实时 http://d1.weather.com.cn/sk_2d/101010100.html?_=1573170108252
+const cityCode = '101010100'  
 
+function nowWeather(cityCode){
+  var time = new Date().getTime() 
+  return `http://d1.weather.com.cn/sk_2d/${cityCode}.html?_=${time}`
+}
+
+console.log(nowWeather(cityCode))
+
+const headers = {
+     headers: {
+      referer: `http://www.weather.com.cn/weather1dn/${cityCode}.shtml`, //源域名 Referer: http://www.weather.com.cn/weather1dn/101010100.shtml
+       'Content-Type': 'text/html',
+       'User-Agent':'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Safari/537.36'
+    }
+  }
 
 
 router.prefix('/v1/api');
 router.get('/weather/now/:cityCode', ctx => {
     const cityCode = ctx.params.cityCode
-    return axios.get('http://www.weather.com.cn/data/sk/'+cityCode+'.shtml')
+    return axios.get(nowWeather(cityCode),headers)
         .then(function (response) {
-          const $ = cheerio.load(response)
+            const $ = cheerio.load(response.data,{ decodeEntities:false})
 
-          console.log($)
+        
 
-          // let hasBody = $('body').html()
-          // if(hasBody == ''){
-          //      ctx.body = {
-          //         "msg": "城市代码错误",
-          //         "code": 500
-          //      }
-          //      return
-          // }
+          let timeWeather = $('html body').html().replace('var dataSK = ','')
 
-       
+          let obj = JSON.parse(timeWeather)
+
+          console.log(obj)
+
           ctx.body = {
                       "msg": "操作成功",
                       "code": 200,
-                      "data": {
-                          "list":[]//结果集合
-                         
-
-                      }
+                      "data": obj
                 }
 
     })
